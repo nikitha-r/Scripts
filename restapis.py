890f019f84436ef939f4bf3b94221679d7c3913d
Nikitha <nikithanikitha1111@gmail.com>
2018-12-20 14:45:23 +0530

    put

--- restapis.py
+++ restapis.py
@@ -23,3 +23,28 @@ class DemoApi(Resource):
         except Exception, e:
             logging.info(e)
             raise e
+
+            
+class SetApi(Resource):
+    """API implementation to set."""
+
+    def put(self, id):
+        try:
+            demo_record = DemoModel.query.get(id)
+            if demo_record is not None:
+                demo_record.age = 18
+                db.session.commit()
+                demo_record_dict = {}
+                res_dict = demo_record.__dict__
+                res_dict.pop('_sa_instance_state')
+                for key,value in res_dict.iteritems():
+                    if (type(value) == decimal.Decimal):
+                        value = float(value)
+                    demo_record_dict[key] = value
+                return jsonify(demo_record_dict)
+            else:
+                logging.info('Given  id does not exist.')
+                return {'PUT': 'Given  id does not exist.'}
+        except Exception, e:
+            logging.info('Failed to set the age. ')
+            raise e
