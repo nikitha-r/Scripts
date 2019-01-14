2e8fb3636f95c072e0dfb6fcdaf3846fa26956f6
Nikitha <nikithanikitha1111@gmail.com>
2019-01-14 16:38:03 +0530

    Path issues fix added.

---
+++ path_issue_fix.py
@@ -0,0 +1,7 @@
+path = os.path.abspath(os.path.join(os.path.dirname('__file__'), '..'))
+export PYTHONPATH =$PYTHONPATH: / Users / nr012 / Desktop / temp3 / 
+
+import sys
+import os
+path = os.path.abspath(os.path.join(os.path.dirname('__file__'), '..'))
+sys.path.insert(0, path)
