7d93edb57506419e7be0495b611a388aa0e62698
Nikitha <nikithanikitha1111@gmail.com>
2019-02-26 16:32:48 +0530

    Create standard_appengine.py

---
+++ standard_appengine.py
@@ -0,0 +1,5 @@
+App engine
+-------------
+1. Create app_engine.py where it adds lib folder.
+2. Feed in all the packages to lib.
+3. If your connecting to database then make sure you use cloud sql and instance name.
