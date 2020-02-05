742007945decd6cbb9cd39d644e74efb07bc2b43
Nikitha <nikithanikitha1111@gmail.com>
2020-02-05 15:01:24 +0530

    list_array_dict.jsx

---
+++ list_array_dict.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        cart: ["A", "B"],
+        shop: [
+            {id: 35, name: 'jumper', color: 'red', price: 20},
+            {id: 42, name: 'shirt', color: 'blue', price: 15},
+            {id: 56, name: 'pants', color: 'green', price: 25},
+            {id: 71, name: 'socks', color: 'black', price: 5},
+            {id: 72, name: 'socks', color: 'white', price: 5},
+        ]
+    };
+  }
+  render() {
+   const items = this.state.shop.map((item, key) =>
+        <li key={item.id}>{item.name}</li>
+    );  
+   const items1 = this.state.cart.map((item) =>
+        <li>{item}</li>
+    );
+
+
+    return (
+      <div>
+      {items}{items1}
+      </div>
+    );
+  }
+}
+
+export default App;
