1dfc4caa3f22ba0500d143f5eab205a13d03b5c9
Nikitha <nikithanikitha1111@gmail.com>
2020-02-27 17:10:31 +0530

    Create Download.py

---
+++ Download.py
@@ -0,0 +1,17 @@
+Install Brew
+------------
+https://osxdaily.com/2018/03/07/how-install-homebrew-mac-os/
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+Adding SSH
+-----------
+ssh-keygen -t rsa -C "nikitha.r@pwc.com"
+cat < ~/.ssh/id_rsa.pub
+  -> Copy the entire thing to add new ssh or public key(title can be given anything)
+git clone git@ssh.dev...
+
+
+Download Git
+---------------
+https://sourceforge.net/projects/git-osx-installer/
